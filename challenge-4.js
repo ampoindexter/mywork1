952c93dcce3dc674436b0292623ae52c51f3f997
Alicia Poindexter <alcampbel@gmail.com>
2015-09-25 19:54:34 -0400

    Counter function challenge

---
+++ challenge-4.js
@@ -0,0 +1,9 @@
+var superCounter = function(string) {
+	var wordCount = string.split(" ").length;
+	var charCount = string.length;
+	var spaceCount = wordCount - 1;
+	var avgWordLength = (charCount - spaceCount) / wordCount;
+	console.log("\"words\": " + wordCount + ", " + "\"chars\": " + charCount + ", " + "\"avgLength\": " + avgWordLength + ", " + "\"spaces\": " + spaceCount)
+};
+
+superCounter("Count me in");
