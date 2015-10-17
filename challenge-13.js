cf456c10d70be5f451d1c257ec8c7fb434414594
Alicia Poindexter <alcampbel@gmail.com>
2015-10-16 22:47:11 -0400

    jQuery and the DOM challenge

---
+++ challenge-13.js
@@ -0,0 +1,26 @@
+function getRandomColor() {
+	var letters = '0123456789ABCDEF'.split('');
+	var hexTripletColor = '#';
+	for (var i = 0; i < 6; i++) {
+		hexTripletColor += letters[Math.floor(Math.random()*16)];
+	}
+	return hexTripletColor;
+}
+
+function invertColor(hexTripletColor) {
+    var color = hexTripletColor;
+    color = color.substring(1);
+    color = parseInt(color, 16);
+    color = 0xFFFFFF ^ color;
+    color = color.toString(16);
+    color = ("000000" + color).slice(-6);
+    color = "#" + color;
+    return color;
+}
+
+$(document).ready(function() {
+	$('div').each(function() {
+		$(this).css('background-color', getRandomColor());
+		$(this).css('color', invertColor(getRandomColor()));
+	});
+});
