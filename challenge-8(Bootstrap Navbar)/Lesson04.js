732d22a077bce087f8b962b7e1aef578a4245644
Alicia Poindexter <alcampbel@gmail.com>
2015-10-20 15:28:47 -0400

    Navbar mockup challenge

---
+++ challenge-8(Bootstrap Navbar)/Lesson04.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('#light').on('click', function() {
+		$('body').css('background-color', '#ffffff');
+		$('body').css('color', '#000000');
+	});
+	$('#dark').on('click', function() {
+		$('body').css('background-color', '#000000');
+		$('body').css('color', '#ffffff');
+	});
+	$('.nav-tabs').click(function(e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+});
